e3c5d75c0293482d284982811ab7178dbecc704e
mcone880 <79989621+mcone880@users.noreply.github.com>
2023-01-19 09:56:09 -0700

    Some Enemies

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,7 @@ public class Player : MonoBehaviour
 	public event UnityAction OnDeath;
 
 	private Rigidbody2D m_RigidBody;
+	private float xInput;
 
 	[SerializeField]
 	private float m_Speed = 2;
@@ -41,41 +42,50 @@ public class Player : MonoBehaviour
 		m_StartHeight = transform.position.y;
 	}
 
+	private void Update()
+    {
+		xInput = Input.GetAxisRaw("Horizontal");
+	}
+
 	private void FixedUpdate()
 	{
-		float x = Input.GetAxisRaw("Horizontal");
 		Vector3 pos = transform.position;
-		pos.x += x * m_Speed * Time.deltaTime;
+		pos.x += xInput * m_Speed * Time.deltaTime;
 		pos.y = m_StartHeight;
 		transform.position = pos;
 	}
 
-	private void OnCollisionEnter2D(Collision2D collision)
-	{
-		if (collision.gameObject.tag == "Wall") return;
+	public void OnTriggerEnter2D(Collider2D col)
+    {
+		switch (col.gameObject.tag)
+		{
+			case "BasicEnemy":
+				Hit();
+				break;
+			case "MustMoveEnemy":
+				if (xInput == 0) Hit();
+				break;
+			case "StayStillEnemy":
+				if (xInput > 0) Hit();
+				break;	
+		}
+	}
 
+    private void Hit()
+	{
 		lives--;
 
-        if (lives < 0)
-		{
-			GameObject particle = Instantiate(m_HitParticals);
+		if(lives > 0)
+        {
+			GameObject hitParticle = Instantiate(m_HitParticals);
+			hitParticle.transform.position = transform.position;
+        } 
+		else
+        {
+			OnDeath.Invoke();
+			GameObject particle = Instantiate(m_DeathParticals);
 			particle.transform.position = transform.position;
+			Destroy(gameObject);
 		}
-		
-        if (lives<= 0)
-		{
-			Die();
-		}
-	}
-
-	private void Die()
-	{
-		OnDeath.Invoke();
-		GameObject particle = Instantiate(m_DeathParticals);
-		particle.transform.position = transform.position;
-		Destroy(gameObject);
 	}
 }
-
-
-// walls don't kill and 3 lives 
